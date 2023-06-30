de6f9f064a30aa5a15c1fd4b906e84a42eff6bdf
Kasparas <kasparas.baniulis@gmail.com>
2023-06-30 20:48:59 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Check from './Check/Check'
+import RootLayout from './RootLayout/RootLayout'
+
+function App() {
+  return (
+    // <main className="bg-gray-400">
+    //   <h1 className="text-4xl">Something Todo</h1>
+
+    //   <Check />
+    // </main>
+    <RootLayout />
+  )
+}
+
+export default App
---
+++ src/RootLayout/RootLayout.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import clsx from 'clsx'
+import { motion, AnimatePresence } from 'framer-motion'
+import { initialTabs as tabs } from './ingredients.ts'
+import './styles.css'
+
+export default function RootLayout() {
+  const [selectedTab, setSelectedTab] = useState(tabs[0])
+
+  return (
+    <div className="relative mx-auto h-screen max-w-lg">
+      <nav className="absolute bottom-0 left-0 right-0 h-12 w-[inherit] max-w-lg overflow-hidden rounded-lg rounded-b-none max-[512px]:rounded-none md:relative md:h-10 md:rounded-b-lg md:rounded-t-none md:border-b-gray-200">
+        <ul className="flex h-full max-w-lg">
+          {tabs.map(item => (
+            <li
+              key={item.label}
+              className={clsx(
+                'relative flex w-full flex-1 cursor-pointer select-none items-center justify-center px-4 py-2',
+                item === selectedTab ? 'bg-gray-200' : 'bg-white'
+              )}
+              onClick={() => setSelectedTab(item)}
+            >
+              {`${item.icon} ${item.label}`}
+              {item === selectedTab ? (
+                <motion.div
+                  className="absolute bottom-px left-0 right-0 h-px bg-accent"
+                  layoutId="underline"
+                />
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={selectedTab ? selectedTab.label : 'empty'}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {selectedTab ? selectedTab.icon : '😋'}
+          </motion.div>
+        </AnimatePresence>
+      </main>
+    </div>
+  )
+}
