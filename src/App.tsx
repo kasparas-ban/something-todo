ab98a41e30630acc426db604d2fa2451961d1dcc
Kasparas <kasparas.baniulis@gmail.com>
2023-07-01 04:21:52 +0300

    Gave up on page animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useLayoutEffect, useRef } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
 import RootLayout from './RootLayout/RootLayout'
@@ -7,39 +6,17 @@ import './App.css'
 
 function App() {
   const location = useLocation()
-  const prevPathRef = useRef<string | null>(null)
-
-  const idxPrev = tabs.findIndex(tab => tab.path === prevPathRef.current)
-  const idxCurrent = tabs.findIndex(tab => tab.path === location.pathname)
-  const moveRight = idxCurrent - idxPrev > 0
-
-  useLayoutEffect(() => {
-    if (prevPathRef) prevPathRef.current = location.pathname
-  }, [location.pathname])
-
-  console.log('moveRight', moveRight)
 
   return (
     <RootLayout>
       <AnimatePresence mode="popLayout">
         <motion.div
-          initial={
-            prevPathRef.current
-              ? { x: moveRight ? 100 : -100, opacity: 0 }
-              : false
-          }
-          animate={{ x: 0, opacity: 1, transition: { delay: 0.2 } }}
-          exit={{ x: moveRight ? -100 : 100, opacity: 0 }}
-          transition={{ duration: 0.2 }}
           key={location.pathname}
+          initial={{ x: 300, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -300, opacity: 0 }}
         >
-          <Routes location={location}>
-            {routes}
-            {/* <Route index element={<HomeTab />} />
-            <Route path="books" element={<BooksTab />} />
-            <Route path="info" element={<InfoTab />} /> */}
-            {/* <Route path="*" element={<NoMatch />} /> */}
-          </Routes>
+          <Routes location={location}>{routes}</Routes>
         </motion.div>
       </AnimatePresence>
     </RootLayout>
