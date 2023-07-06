d7bbdd5b8396b05c2156c76c10d2b98e37dc5d58
Kasparas <kasparas.baniulis@gmail.com>
2023-07-06 03:55:16 +0300

    Update test

--- src/Tabs/HomeTab.tsx
+++ src/Tabs/HomeTab.tsx
@@ -1,5 +1,7 @@
 export default function HomeTab() {
   return (
-    <div className="flex h-full items-center justify-center text-9xl">🏠</div>
+    <div className="flex h-full items-center justify-center text-9xl">
+      🏠HOME
+    </div>
   )
 }
