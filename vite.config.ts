e8fa46fe7685d47f1294712995042afebbd432b6
Kasparas <kasparas.baniulis@gmail.com>
2023-07-10 05:26:40 +0300

    Disabled custom sw

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'custom-sw.ts',
+      // strategies: 'injectManifest',
+      // srcDir: 'src',
+      // filename: 'custom-sw.ts',
       registerType: 'prompt',
       devOptions: {
         enabled: true,
-        type: 'module',
-        navigateFallback: 'index.html',
+        // type: 'module',
+        // navigateFallback: 'index.html',
       },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
