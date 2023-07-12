e4057fbd578fae27bd1eeac63073930084a7547b
Kasparas <kasparas.baniulis@gmail.com>
2023-07-12 10:10:11 +0300

    Updated manifest to meet the requirements

--- vite.config.ts
+++ vite.config.ts
@@ -18,15 +18,31 @@ export default defineConfig({
       },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
+        id: '/',
         name: 'Something Todo',
         short_name: 'SmthTodo',
         description: 'Track your work progress',
         theme_color: '#00a6ed',
+        display_override: ['window-controls-overlay', 'minimal-ui'],
+        display: 'standalone',
+        edge_side_panel: {},
+        handle_links: 'auto',
+        launch_handler: {
+          client_mode: 'navigate-existing',
+        },
+        categories: ['tools', 'libraries_and_demo'],
+        orientation: 'portrait',
+        dir: 'ltr',
+        file_handlers: [],
+        prefer_related_applications: false,
+        protocol_handlers: [],
+        related_applications: [],
         icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any',
           },
           {
             src: 'pwa-512x512.png',
@@ -38,6 +54,14 @@ export default defineConfig({
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshot_1.jpg',
+            sizes: '1336x748',
+            type: 'image/png',
+            platform: 'narrow',
           },
         ],
       },
